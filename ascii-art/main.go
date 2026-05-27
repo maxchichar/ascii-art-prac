927aa523c4e7f38a7300d7a14240fbd232ac0ff3
maxchichar <maxwellchibuezecharles@gmail.com>
2026-05-27 11:49:39 +0100

    worked on banner switching

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -8,24 +8,76 @@ import(
 
 
 func main()  {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		fmt.Println("usage: go run . [STRING] [BANNER]")
 	}
 	
-	data, err := os.ReadFile("standard.txt")
-	if err != nil{
-		fmt.Println("error: ", err)
-		return
-	}
+	bannername := os.Args[2]
 
-	content := string(data)
-	lines := strings.Split(content, "\n")
-
-	for row := 0; row < 8; row++ {
-		for _, ch := range os.Args[1]{
-			ascii := (ch - 32) * 9 + 1
-			fmt.Print(lines[int(ascii) + row])
+	var banner = bannername + ".txt"
+	
+	
+	switch bannername {
+	case "thinkertoy":
+		data, err:= os.ReadFile(banner)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
 		}
-		fmt.Println()
+
+		content := string(data)
+		lines := strings.Split(content, "\n")
+
+		for row := 0; row < 8; row++ {
+			for _, ch := range os.Args[1]{
+				ascii := (ch - 32) * 9 + 1
+				fmt.Print(lines[int(ascii) + row])
+			}
+			fmt.Println()
+		}
+
+	case "shadow":
+		data, err := os.ReadFile(banner)
+		if err != nil{
+			fmt.Println("error: ", err)
+			return
+		}
+	
+	
+		content := string(data)
+		lines := strings.Split(content, "\n")
+	
+		for row := 0; row < 8; row++ {
+			for _, ch := range os.Args[1]{
+				ascii := (ch - 32) * 9 + 1
+				fmt.Print(lines[int(ascii) + row])
+			}
+			fmt.Println()
+		}
+	case "standard":
+		data, err := os.ReadFile(banner)
+		if err != nil{
+			fmt.Println("error: ", err)
+			return
+		}
+		
+		
+		content := string(data)
+		chunk := strings.ReplaceAll(content, "\"\n", "\n")
+		lines := strings.Split(chunk, "\n")
+	
+		for row := 0; row < 8; row++ {
+			for _, ch := range os.Args[1]{
+				ascii := (ch - 32) * 9 + 1
+				fmt.Print(lines[int(ascii) + row])
+			}
+			fmt.Println()
+		}
+	default:
+		fmt.Println("usage: go run . [STRING] [BANNER]")
 	}
+	
+	
 }
+
+// func Processor()
