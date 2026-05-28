ea10a6b409fcd006a549155da1fef9b6ae963bd1
maxchichar <maxwellchibuezecharles@gmail.com>
2026-05-28 13:24:47 +0100

    removed repitition in code and also handle empty string

--- ascii-art/main.go
+++ ascii-art/main.go
@@ -7,77 +7,60 @@ import(
 )
 
 
+var availableBannerMessage = `
+Available Banners: "standard", "shadow", "thinkertoy"
+
+Usage: 
+go run . "Hello" "standard"
+go run . "Hello" "shadow"
+go run . "Hello" "thinkertoy"
+`
+
+
 func main()  {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 2 || len(os.Args) > 3 {
 		fmt.Println("usage: go run . [STRING] [BANNER]")
+		return
+	}
+
+	var input = os.Args[1]
+
+	var bannername = "standard"
+
+	if len(os.Args) == 3 {
+		bannername = os.Args[2]
+	}
+
+	lines, err := LoadBanner(bannername + ".txt")
+	if err != nil {
+		fmt.Print("error:", err)
+		fmt.Println(availableBannerMessage)
+		return
+	}
+
+	if input == "" {
+		return
 	}
 	
-	bannername := os.Args[2]
+	// userInput := strings.ReplaceAll(input)
 
-	var banner = bannername + ".txt"
-	
-	
-	switch bannername {
-	case "thinkertoy":
-		data, err:= os.ReadFile(banner)
-		if err != nil {
-			fmt.Println("error:", err)
-			return
+	for row := 0; row < 8; row++ {
+		for _, char := range input{
+			ascii_char := (char - 32) * 9 + 1
+			fmt.Print(lines[int(ascii_char) + row])
 		}
-
-		content := string(data)
-		lines := strings.Split(content, "\n")
-
-		for row := 0; row < 8; row++ {
-			for _, ch := range os.Args[1]{
-				ascii := (ch - 32) * 9 + 1
-				fmt.Print(lines[int(ascii) + row])
-			}
-			fmt.Println()
-		}
-
-	case "shadow":
-		data, err := os.ReadFile(banner)
-		if err != nil{
-			fmt.Println("error: ", err)
-			return
-		}
-	
-	
-		content := string(data)
-		lines := strings.Split(content, "\n")
-	
-		for row := 0; row < 8; row++ {
-			for _, ch := range os.Args[1]{
-				ascii := (ch - 32) * 9 + 1
-				fmt.Print(lines[int(ascii) + row])
-			}
-			fmt.Println()
-		}
-	case "standard":
-		data, err := os.ReadFile(banner)
-		if err != nil{
-			fmt.Println("error: ", err)
-			return
-		}
-		
-		
-		content := string(data)
-		chunk := strings.ReplaceAll(content, "\"\n", "\n")
-		lines := strings.Split(chunk, "\n")
-	
-		for row := 0; row < 8; row++ {
-			for _, ch := range os.Args[1]{
-				ascii := (ch - 32) * 9 + 1
-				fmt.Print(lines[int(ascii) + row])
-			}
-			fmt.Println()
-		}
-	default:
-		fmt.Println("usage: go run . [STRING] [BANNER]")
+		fmt.Println()
 	}
-	
-	
 }
 
-// func Processor()
+
+func LoadBanner(filename string) ([]string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	
+	lines := strings.Split(strings.ReplaceAll(string(data), "\r\n", "\n"),"\n")
+	
+	return lines, nil
+}
